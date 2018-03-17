5c51df4bcd6e61b1de90d2a1795198ae89b26e93
elizabeth <elizabeth@elizabeth-HP>
2018-03-17 12:55:22 +0100

    modifié

---
+++ src/ch/dsl/bachelor/git/Main.java
@@ -0,0 +1,14 @@
+package ch.dsl.bachelor.git;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		TestGit myfirstTestGit=new TestGitUpdate("My Parameter");
+		System.out.println(myfirstTestGit.returnTheString());
+		
+		
+	}
+
+}
